37f4047a4eb4a81d01cc4c91d2c58afed8777d43
MicheleSr <mikefender@cryptolab.net>
2014-03-22 16:34:43 +0100

    Divided program in more source files.

--- src/simple_osc.c
+++ src/simple_osc.c
@@ -1,4 +1,5 @@
-/*
+/*  simple_osc.c main() implementation source file
+ 
     Copyright (C) 2004 Ian Esten
     Copyright (C) 2014 Michele Sorcinelli 
     
@@ -27,165 +28,31 @@
 #include <jack/jack.h>
 #include <jack/midiport.h>
 
-/* number of implemented waveforms */
-#define FORMS 4 
-
-/* frequence of A4 */
-#define TUNING 440.0 
-
-/* gain adjustment for waveforms */
-#define GAIN_SIN 1.0
-#define GAIN_SQR 0.85
-#define GAIN_SAW 0.885
-#define GAIN_TRI 1.01
+#include "lib/shell_ui.h"
+#include "lib/synth.h"
 
 /* global vars */
-
-typedef jack_default_audio_sample_t sample_t;
 
 jack_port_t *in_p;
 jack_port_t *out_p;
 
 sample_t ramp = 0.0;
 sample_t note_on = 0;
-sample_t note_frqs[128];
-double max_amplitude = 0.5;
 unsigned char note = 0, active_notes[128];
-char waveform = 'a';
-int fi = 20; /* fourier iterations */
 
 /* function declaration */
 
-int check_waveform(char);
 int process(jack_nframes_t, void*);
 int srate(jack_nframes_t, void*);
 int note_is_active(unsigned char);
 int search_active_note(unsigned char);
 int active_notes_is_empty();
 unsigned char search_highest_active_note();
-void set_waveform();
-void set_fi();
-void print_help_message();
-void calc_note_frqs(sample_t); 
 void jack_shutdown(void *); 
-void shell_loop(char *);
 void add_active_note(unsigned char );
 void del_active_note(unsigned char ); 
 
 /* function definition */
-
-int check_waveform(char c) {
-  char valid_forms[FORMS];
-  int i, v;
-  for (i=0; i < FORMS; i++) 
-    valid_forms[i] = (i + 'a');
-  for (i=0; i < FORMS && c != valid_forms[i]; i++); 
-  return (i < FORMS); 
-}
-
-void set_waveform() {
-  char name[10];
-  char c;
-
-  while(getchar() != '\n');
-
-  printf("Select waveform: \n"
-         "a) sine\n"
-         "b) square\n"
-         "c) sawtooth\n"
-         "d) triangle\n"
-         "waveform: ");
-
-  c = getchar();
-  while (!check_waveform(c)) {
-    printf("invalid waveform\n");
-    while(getchar() != '\n');
-    printf("waveform: ");
-    scanf("%c", &c);
-  }
-
-  waveform = c;
-  switch(c) {
-    case 'a':
-      strcpy(name, "sine");
-      break;
-    case 'b':
-      strcpy(name, "square");
-      break;
-    case 'c':
-      strcpy(name, "sawtooth");
-      break;
-    case 'd':
-      strcpy(name, "triangle");
-      break;
-  }
-
-  printf("Changed waveform: %s\n", name);
-}
-
-void set_fi() {
-  do { 
-    printf("N° of fourier iterations: ");
-    scanf("%d", &fi);
-  }  
-  while (fi < 1); 
-}
-
-void set_amplitude() {
-  printf("Set new max amplitude: ");
-  scanf("%lf", &max_amplitude);
-  printf("New amplitude = %.2lf\n",  max_amplitude);
-  if (max_amplitude < 0)
-    printf("A < 0 => Inverted Phase\n"); 
-}
-
-void print_help_message() {
-  printf("h -> help\n"
-         "W -> change waveform\n" 
-         "A -> set amplitude (volume)\n"
-         "i -> set number of fourier iterations\n"
-         "The number of fourier iterations is related to the precision of the waveform... more is higher, and more cpu power is needed... you can set at around 20 or higher if you want (will your ear notice the difference?).\n"); 
-}
-
-void shell_loop(char *name) {
-  char c;
-  printf("Hi! if you need help, type h\n"
-         "To close the synth, type q or ^D\n%s: ", name);
-
-  /* shell loop */
-  while((c = getchar()) != 'q' && c != EOF) 
-  {
-      switch(c)
-      {
-        case 'A':
-          set_amplitude();
-          break;
-        case 'W':
-          set_waveform();
-          break;
-        case 'h':
-          print_help_message();
-          break;
-        case 'i':
-          set_fi();
-        case '\n':
-        case ' ':
-        case '\t':
-          break;
-        default:
-          printf("invalid input\n");
-          break;
-      }
-      if (c != '\n' && c != '\t' && c != ' ')
-        printf("%s: ", name);
-  }
-}
-
-void calc_note_frqs(sample_t srate) {
-  int i;
-  for(i=0; i<128; i++)
-    note_frqs[i] = (TUNING / 16.0) * pow(2, (((sample_t)i - 9.0) / 12.0)) / srate;
-}
 
 int process(jack_nframes_t nframes, void *arg) {
   int i;
@@ -239,22 +106,16 @@ int process(jack_nframes_t nframes, void *arg) {
       int k;
       switch(waveform) {
         case 'a':
-          out[i] = GAIN_SIN * max_amplitude * note_on * sin(2*M_PI*ramp);
+          out[i] = note_on * sine_w(ramp);
           break;
         case 'b':
-          for (k=1; k <= fi; k++) 
-            x += (sin(2*M_PI*ramp*(2*k -1))/(2*k -1));
-          out[i] = GAIN_SQR * max_amplitude * note_on*4*x/M_PI;
+          out[i] = note_on * square_w(ramp);
           break;
         case 'c':
-          for (k=1; k <= fi; k++)
-            x += pow(-1,k)/k * sin(2*M_PI*ramp*k); 
-          out[i] = GAIN_SAW * note_on * max_amplitude * x *(-2)/M_PI;
+          out[i] = note_on * sawtooth_w(ramp); 
           break;
         case 'd':
-          for (k=0; k < fi; k++)
-            x += pow(-1,k) * sin(2*M_PI*ramp*(2*k+1))/(2*k+1)/(2*k+1); 
-          out[i] = GAIN_TRI *note_on * max_amplitude * x * 8 / M_PI / M_PI;
+          out[i] = note_on * triangle_w(ramp);
           break;
       }
     }
